73c779017c18d736dbf2e4b55ba4a7a55ea36ff7
MarkoIvanetic <manna.grad@gmail.com>
2021-03-16 00:02:07 +0100

    calendar component done

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useEffect, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-
+import { connect } from 'react-redux'
+import { getAllHabits } from '@api'
 import moment from 'moment'
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core'
 
 function getPreviousMonday() {
-    var date = new Date()
-    var day = date.getDay()
-    var prevMonday = new Date()
+    const date = new Date()
+    const day = date.getDay()
+    const prevMonday = new Date()
     if (date.getDay() == 0) {
         prevMonday.setDate(date.getDate() - 7)
     } else {
@@ -18,23 +20,64 @@ function getPreviousMonday() {
 }
 
 const generateWeekCalendar = () => {
-    let calendar = []
-    const monday = moment(getPreviousMonday())
+    const calendar = []
+    const monday = moment(getPreviousMonday()).format()
 
-    for (let i = 1; i < 7; i++) {
+    for (let i = 0; i < 7; i++) {
         calendar.push({
-            date: monday.add(i, 'days'),
+            date: moment(monday).add(i, 'days').format('dddd'),
             entries: []
         })
     }
+
+    return calendar
 }
 
-const Calendar = props => {
-    return <div></div>
+const weekCalendar = generateWeekCalendar()
+
+const Calendar = ({ habits }) => {
+    return (
+        <div>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Habit</TableCell>
+                            <TableCell>Metric</TableCell>
+                            <TableCell>Rating</TableCell>
+                            {weekCalendar.map(day => (
+                                <TableCell>{day.date}</TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {habits.map(row => (
+                            <TableRow key={row.id}>
+                                <TableCell align="right">{row.title}</TableCell>
+                                <TableCell align="right">{row.metric}</TableCell>
+                                <TableCell align="right">{row.points}</TableCell>
+                                {weekCalendar.map(day => (
+                                    <TableCell align="right">0</TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    )
 }
 
 Calendar.propTypes = {}
 
-Calendar.defaultProps = {}
+Calendar.defaultProps = {
+    habits: []
+}
 
-export default Calendar
+const mapStateToProps = state => {
+    return {
+        habits: state.habits
+    }
+}
+
+export default connect(mapStateToProps)(Calendar)
